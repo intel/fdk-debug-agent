76091a7318da351a4da6f6bd2300d1be8c4c4f29
Mathieu Soulard <mathieu.soulard@intel.com>
2015-06-09 10:05:26 +0200

    Add cavs::System
    This patch introduces a class to represent the cAVS System.
    
    The cavs::System is currently only composed of a Logger.
    
    Change-Id: I0ea8747ca97f288931664558a1e5af0120b4c20e
    Signed-off-by: Mathieu Soulard <mathieu.soulard@intel.com>

---
+++ components/cAVS/include/cAVS/System.hpp
@@ -0,0 +1,75 @@
+/*
+********************************************************************************
+*                              INTEL CONFIDENTIAL
+*   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+*   The source code contained  or  described herein and all documents related to
+*   the source code ("Material") are owned by Intel Corporation or its suppliers
+*   or licensors.  Title to the  Material remains with  Intel Corporation or its
+*   suppliers and licensors. The Material contains trade secrets and proprietary
+*   and  confidential  information of  Intel or its suppliers and licensors. The
+*   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+*   treaty  provisions. No part of the Material may be used, copied, reproduced,
+*   modified, published, uploaded, posted, transmitted, distributed or disclosed
+*   in any way without Intel's prior express written permission.
+*   No license  under any  patent, copyright, trade secret or other intellectual
+*   property right is granted to or conferred upon you by disclosure or delivery
+*   of the Materials,  either expressly, by implication, inducement, estoppel or
+*   otherwise.  Any  license  under  such  intellectual property  rights must be
+*   express and approved by Intel in writing.
+*
+********************************************************************************
+*/
+#pragma once
+
+#include "cAVS/Driver.hpp"
+#include <memory>
+#include <stdexcept>
+
+namespace debug_agent
+{
+namespace cavs
+{
+
+/**
+ * The cAVS System
+ */
+class System final
+{
+public:
+    class Exception : public std::logic_error
+    {
+    public:
+        explicit Exception(const std::string& what)
+        : std::logic_error(what)
+        {}
+    };
+
+    /**
+     * @throw System::Exception
+     */
+    System();
+
+    /**
+     * Set log parameters
+     * @param[in] parameters Log parameters to be set
+     */
+    void setLogParameters(Logger::Parameters &parameters);
+
+    /**
+     * Get log parameters
+     * @return current log parameters
+     */
+    Logger::Parameters getLogParameters();
+
+private:
+    /* Make this class non copyable */
+    System(const System &) = delete;
+    System & operator=(const System &) = delete;
+
+    std::unique_ptr<Driver> mDriver;
+};
+
+}
+}
+
+
