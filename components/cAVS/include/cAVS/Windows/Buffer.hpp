929fc4ff70e05d180ebf661f580a0fb30b82393c
Thomas Cahuzac <thomasx.cahuzac@intel.com>
2015-06-18 09:37:17 +0200

    Introducing Windows device io control interface
    The windows driver has to be mocked up.
    
    Therefore this patch introduces a device io control
    interface, it will be implemented by the mocked driver
    and the real driver.
    
    Change-Id: I7d8787abd25fddcd2855d263bdaeb0c2f2d07ed4
    Signed-off-by: Thomas Cahuzac <thomasx.cahuzac@intel.com>

---
+++ components/cAVS/include/cAVS/Windows/Buffer.hpp
@@ -0,0 +1,161 @@
+/*
+ ********************************************************************************
+ *                              INTEL CONFIDENTIAL
+ *   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+ *   The source code contained  or  described herein and all documents related to
+ *   the source code ("Material") are owned by Intel Corporation or its suppliers
+ *   or licensors.  Title to the  Material remains with  Intel Corporation or its
+ *   suppliers and licensors. The Material contains trade secrets and proprietary
+ *   and  confidential  information of  Intel or its suppliers and licensors. The
+ *   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+ *   treaty  provisions. No part of the Material may be used, copied, reproduced,
+ *   modified, published, uploaded, posted, transmitted, distributed or disclosed
+ *   in any way without Intel's prior express written permission.
+ *   No license  under any  patent, copyright, trade secret or other intellectual
+ *   property right is granted to or conferred upon you by disclosure or delivery
+ *   of the Materials,  either expressly, by implication, inducement, estoppel or
+ *   otherwise.  Any  license  under  such  intellectual property  rights must be
+ *   express and approved by Intel in writing.
+ *
+ ********************************************************************************
+ */
+
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <assert.h>
+
+namespace debug_agent
+{
+namespace cavs
+{
+namespace windows
+{
+
+/** Simple buffer class that cannot be resized, used for windows ioctl calls */
+class Buffer
+{
+public:
+    /** @param[in] size the buffer byte size */
+    explicit Buffer(std::size_t size) : mBuffer(size)
+    {
+        /* Setting the initial memory content using a non 0 value. This is required by
+         * the mocked up driver, because it checks that buffers are the expected ones,
+         * and sometime the user doesn't set the full buffer content. Therefore without this
+         * memory set, areas not set by the user are unpredictables.
+         *
+         * This may lead to performance issue. If it is the case, consider activating
+         * this memory set only in mocking case. */
+        memset(getPtr(), 0xFF, getSize());
+    }
+
+    explicit Buffer(const Buffer &other)
+    {
+        mBuffer = other.mBuffer;
+    }
+
+    virtual ~Buffer() {}
+
+    void *getPtr()
+    {
+        return mBuffer.data();
+    }
+
+    const void *getPtr() const
+    {
+        return mBuffer.data();
+    }
+
+    std::size_t getSize() const
+    {
+        return mBuffer.size();
+    }
+
+    bool operator == (const Buffer& other) const
+    {
+        return mBuffer == other.mBuffer;
+    }
+
+    bool operator != (const Buffer& other) const
+    {
+        return !(*this == other);
+    }
+
+private:
+
+    Buffer &operator=(const Buffer &other) = delete;
+
+    std::vector<uint8_t> mBuffer;
+};
+
+/** This buffer maps a typed instance
+ * For instance:
+ *
+ * struct MyStruct {
+ *     int member;
+ * }
+ *
+ * TypedBuffer<MyStruct> buffer();
+ *
+ * // underlying type members can be accessed using -> operator
+ * buffer->member = 2;
+ *
+ * //another way consists in getting a reference to the underlying type using getContent() method
+ * MyStruct &struct = buffer.getContent();
+ * struct.member = 2;
+ *
+ * The constructor takes an optional size parameter: this size can be used to allocate a memory
+ * area greater than the underlying structure, this is often useful for ioctl structures.
+ * For instance this one:
+ *
+ * typedef struct _Intc_Set_Parameter
+ * {
+ *    NTSTATUS Status;
+ *    UCHAR Data[1];
+ * } Intc_Set_Parameter;
+ *
+ * The member 'Data' of the struct Intc_Set_Parameter matches the type "UCHAR[1]", but in reality
+ * the signification of this type is "char array with an arbitrary size".So if the real 'Data'
+ * member size is 128:
+ *
+ * TypedBuffer<_Intc_Set_Parameter> buffer(sizeof(_Intc_Set_Parameter) + (128-1) * sizeof(UCHAR));
+ * buffer->Data[120] = 2; // index valid in range 0..127
+ *
+ * @tparam T the underlying buffer type
+ */
+template <typename T>
+class TypedBuffer : public Buffer
+{
+public:
+    /** @param[in] size The size of the buffer, shall be >= sizeof(T).
+     *                  By default size = sizeof(T) */
+    explicit TypedBuffer(std::size_t size = sizeof(T)) : Buffer(size)
+    {
+        assert(size >= sizeof(T));
+    }
+
+    /**
+     * @param[in] value The value used for buffer initialization
+     * @param[in] size The size of the buffer, shall be >= sizeof(T).
+     *                  By default size = sizeof(T) */
+    explicit TypedBuffer(const T &value, std::size_t size = sizeof(T)) : Buffer(size)
+    {
+        assert(size >= sizeof(T));
+        memcpy(getPtr(), &value, sizeof(T));
+    }
+
+    T &getContent()
+    {
+        return *static_cast<T *>(getPtr());
+    }
+
+    T* operator->()
+    {
+        return &getContent();
+    }
+};
+
+}
+}
+}
---
+++ components/cAVS/include/cAVS/Windows/Device.hpp
@@ -0,0 +1,73 @@
+/*
+ ********************************************************************************
+ *                              INTEL CONFIDENTIAL
+ *   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+ *   The source code contained  or  described herein and all documents related to
+ *   the source code ("Material") are owned by Intel Corporation or its suppliers
+ *   or licensors.  Title to the  Material remains with  Intel Corporation or its
+ *   suppliers and licensors. The Material contains trade secrets and proprietary
+ *   and  confidential  information of  Intel or its suppliers and licensors. The
+ *   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+ *   treaty  provisions. No part of the Material may be used, copied, reproduced,
+ *   modified, published, uploaded, posted, transmitted, distributed or disclosed
+ *   in any way without Intel's prior express written permission.
+ *   No license  under any  patent, copyright, trade secret or other intellectual
+ *   property right is granted to or conferred upon you by disclosure or delivery
+ *   of the Materials,  either expressly, by implication, inducement, estoppel or
+ *   otherwise.  Any  license  under  such  intellectual property  rights must be
+ *   express and approved by Intel in writing.
+ *
+ ********************************************************************************
+ */
+
+#pragma once
+
+#include "cAVS/Windows/Buffer.hpp"
+#include <exception>
+#include <string>
+#include <inttypes.h>
+#include <stdexcept>
+
+namespace debug_agent
+{
+namespace cavs
+{
+namespace windows
+{
+
+/** This class abstracts a Windows device that supports io control.
+ * The ioctl API wraps the windows DeviceIoControl function, see:
+ * https://msdn.microsoft.com/en-us/library/windows/desktop/aa363216(v=vs.85).aspx
+ */
+class Device
+{
+public:
+    class Exception : public std::logic_error
+    {
+    public:
+        Exception(const std::string &msg) : std::logic_error(msg.c_str()) {}
+    };
+
+    /** @throw Exception if the device initialization has failed */
+    Device() {}
+    virtual ~Device() {}
+
+    /** Perform an IO control
+    * @param[in] ioControlCode the IO control code
+    * @param[in] inputBuffer the input buffer. This parameter is optional, use nullptr if the
+                             input buffer is not required.
+    * @param[in,out] outputBuffer the output buffer (basically this buffer can be used as input
+    *                             buffer too). This parameter is optional, use nullptr if the
+                                  output buffer is not required.
+    * @throw Exception if the io control has failed
+    */
+    virtual void ioControl(uint32_t ioControlCode, const Buffer *input, Buffer *output) = 0;
+
+private:
+    Device(const Device &) = delete;
+    Device &operator=(const Device &) = delete;
+};
+
+}
+}
+}
