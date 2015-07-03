ca5e8c67d8c35908ebd3cabae29741f20f5cea4b
Thomas Cahuzac <thomasx.cahuzac@intel.com>
2015-07-03 14:28:12 +0200

    Introducing cAVS test framework library
    Some cAVS test features are needed by both:
    - the cAVS unit tests
    - the DebugAgent functional tests.
    
    So this patch intoduces a cAVS test framework static
    library that provides a helper class to feed the mocked
    device test vector.
    
    Change-Id: I4c921f47e03216626d2a28b48296316c52805eba
    Signed-off-by: Thomas Cahuzac <thomasx.cahuzac@intel.com>

---
+++ components/cAVS/TestFramework/include/cAVS/Windows/MockedDeviceCommands.hpp
@@ -0,0 +1,118 @@
+/*
+ ********************************************************************************
+ *                              INTEL CONFIDENTIAL
+ *   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+ *   The source code contained  or  described herein and all documents related to
+ *   the source code ("Material") are owned by Intel Corporation or its suppliers
+ *   or licensors.  Title to the  Material remains with  Intel Corporation or its
+ *   suppliers and licensors. The Material contains trade secrets and proprietary
+ *   and  confidential  information of  Intel or its suppliers and licensors. The
+ *   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+ *   treaty  provisions. No part of the Material may be used, copied, reproduced,
+ *   modified, published, uploaded, posted, transmitted, distributed or disclosed
+ *   in any way without Intel's prior express written permission.
+ *   No license  under any  patent, copyright, trade secret or other intellectual
+ *   property right is granted to or conferred upon you by disclosure or delivery
+ *   of the Materials,  either expressly, by implication, inducement, estoppel or
+ *   otherwise.  Any  license  under  such  intellectual property  rights must be
+ *   express and approved by Intel in writing.
+ *
+ ********************************************************************************
+ */
+
+#pragma once
+
+#include "cAVS/FirmwareTypes.hpp"
+#include "cAVS/Windows/DriverTypes.hpp"
+#include "cAVS/Windows/MockedDevice.hpp"
+#include <vector>
+
+namespace debug_agent
+{
+namespace cavs
+{
+namespace windows
+{
+
+/** This class helps to feed a mocked device test vector by adding ioctl commands */
+class MockedDeviceCommands final
+{
+public:
+    MockedDeviceCommands(MockedDevice &device) : mDevice(device) {}
+
+    /** Add a get adsp properties command
+     *
+     * @param[in] returnedDriverStatus the returned driver status
+     * @param[in] returnedFirmwareStatus the returned firmware status
+     * @param[in] returnedProperties the properties returned by the ioctl.
+     *
+     * Note: the returnedProperties parameter is unused if :
+     * - returnedDriverStatus != STATUS_SUCCESS or
+     * - returnedFirmwareStatus != ADSP_IPC_SUCCESS
+     *
+     * @throw MockedDevice::Exception
+     */
+    void addGetAdspPropertiesCommand(NTSTATUS returnedDriverStatus,
+        dsp_fw::Message::IxcStatus returnedFirmwareStatus,
+        const dsp_fw::AdspProperties &returnedProperties);
+
+    /** Add a get module entries command.
+     *
+     * @param[in] returnedDriverStatus the returned driver status
+     * @param[in] returnedFirmwareStatus the returned firmware status
+     * @param[in] returnedEntries the module entries returned by the ioctl.
+     *
+     * Note: the returnedEntries parameter is unused if :
+     * - returnedDriverStatus != STATUS_SUCCESS or
+     * - returnedFirmwareStatus != ADSP_IPC_SUCCESS
+     *
+     * @throw MockedDevice::Exception
+     */
+    void addGetModuleEntriesCommand(NTSTATUS returnedDriverStatus,
+        dsp_fw::Message::IxcStatus returnedFirmwareStatus,
+        const std::vector<dsp_fw::ModuleEntry> &returnedEntries);
+
+    /** Add a get log parameters command
+    *
+    * @param[in] returnedDriverStatus the returned driver status
+    * @param[in] returnedState the log parameters returned by the ioctl
+    *
+    * Note: returnedState is unused if returnedDriverStatus != STATUS_SUCCESS
+    *
+    * @throw MockedDevice::Exception
+    */
+    void addGetLogParametersCommand(NTSTATUS returnedStatus,
+        const driver::FwLogsState &returnedState);
+
+    /** Add a set log parameters command.
+     *
+     * @param[in] returnedDriverStatus the returned driver status
+     * @param[in] expectedState the expected log parameters passed as input buffer to the ioctl
+     *
+     * Note: the expectedState parameter is alwayse used,
+     * even if returnedDriverStatus != STATUS_SUCCESS
+     *
+     * @throw MockedDevice::Exception
+     */
+    void addSetLogParametersCommand(NTSTATUS returnedStatus,
+        const driver::FwLogsState &expectedState);
+
+private:
+    MockedDeviceCommands(const MockedDeviceCommands&) = delete;
+    MockedDeviceCommands &operator=(const MockedDeviceCommands&) = delete;
+
+    /** Template method that adds a module access ioctl
+     *
+     * @tparam FirmwareParameterType the firmware paramerter type, for instance
+     * AdspProperties or ModulesInfo */
+    template <typename FirmwareParameterType>
+    void addGetModuleParameterCommand(dsp_fw::BaseFwParams parameterTypeId,
+        const Buffer &returnedParameterContent, NTSTATUS returnedDriverStatus,
+        dsp_fw::Message::IxcStatus returnedFirmwareStatus);
+
+    MockedDevice &mDevice;
+};
+
+}
+}
+}
---
+++ components/cAVS/TestFramework/src/Windows/MockedDeviceCommands.cpp
@@ -0,0 +1,145 @@
+/*
+ ********************************************************************************
+ *                              INTEL CONFIDENTIAL
+ *   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+ *   The source code contained  or  described herein and all documents related to
+ *   the source code ("Material") are owned by Intel Corporation or its suppliers
+ *   or licensors.  Title to the  Material remains with  Intel Corporation or its
+ *   suppliers and licensors. The Material contains trade secrets and proprietary
+ *   and  confidential  information of  Intel or its suppliers and licensors. The
+ *   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+ *   treaty  provisions. No part of the Material may be used, copied, reproduced,
+ *   modified, published, uploaded, posted, transmitted, distributed or disclosed
+ *   in any way without Intel's prior express written permission.
+ *   No license  under any  patent, copyright, trade secret or other intellectual
+ *   property right is granted to or conferred upon you by disclosure or delivery
+ *   of the Materials,  either expressly, by implication, inducement, estoppel or
+ *   otherwise.  Any  license  under  such  intellectual property  rights must be
+ *   express and approved by Intel in writing.
+ *
+ ********************************************************************************
+ */
+
+#include "cAVS/Windows/MockedDeviceCommands.hpp"
+#include "cAVS/Windows/IoCtlStructureHelpers.hpp"
+
+namespace debug_agent
+{
+namespace cavs
+{
+namespace windows
+{
+
+template <typename FirmwareParameterType>
+void MockedDeviceCommands::addGetModuleParameterCommand(dsp_fw::BaseFwParams parameterTypeId,
+    const Buffer &returnedParameterContent, NTSTATUS returnedDriverStatus,
+    dsp_fw::Message::IxcStatus returnedFirmwareStatus)
+{
+    /* Expected output buffer*/
+    BigCmdModuleAccessIoctlOutput<FirmwareParameterType> expectedOutput(
+        parameterTypeId, returnedParameterContent.getSize());
+
+    /* Returned output buffer*/
+    BigCmdModuleAccessIoctlOutput<FirmwareParameterType> returnedOutput(expectedOutput);
+
+    returnedOutput.getCmdBody().Status = returnedDriverStatus;
+    if (returnedDriverStatus == STATUS_SUCCESS) {
+
+        /* If the driver returns success, set the firmware status*/
+        returnedOutput.getModuleParameterAccess().FwStatus = returnedFirmwareStatus;
+
+        if (returnedFirmwareStatus == dsp_fw::Message::IxcStatus::ADSP_IPC_SUCCESS) {
+
+            /* Setting returned parameter content if the firmware returns success */
+            memcpy(&returnedOutput.getFirmwareParameter(), returnedParameterContent.getPtr(),
+                returnedParameterContent.getSize());
+        }
+    }
+
+    /* Filling expected input buffer */
+    TypedBuffer<driver::Intc_App_Cmd_Header> expectedInput;
+    expectedInput->FeatureID =
+        static_cast<ULONG>(driver::FEATURE_MODULE_PARAMETER_ACCESS);
+    expectedInput->ParameterID = 0; /* only one parameter id for this feature */
+    expectedInput->DataSize = static_cast<ULONG>(expectedOutput.getBuffer().getSize());
+
+    /* Adding entry */
+    mDevice.addIoctlEntry(IOCTL_CMD_APP_TO_AUDIODSP_BIG_GET, &expectedInput,
+        &expectedOutput.getBuffer(), &returnedOutput.getBuffer(), true);
+}
+
+void MockedDeviceCommands::addGetAdspPropertiesCommand(NTSTATUS returnedDriverStatus,
+    dsp_fw::Message::IxcStatus returnedFirmwareStatus,
+    const dsp_fw::AdspProperties &returnedProperties)
+{
+    TypedBuffer<dsp_fw::AdspProperties> buffer(returnedProperties);
+
+    addGetModuleParameterCommand<dsp_fw::AdspProperties>(dsp_fw::BaseFwParams::ADSP_PROPERTIES,
+        buffer, returnedDriverStatus, returnedFirmwareStatus);
+
+}
+
+void MockedDeviceCommands::addGetModuleEntriesCommand(
+    NTSTATUS returnedDriverStatus, dsp_fw::Message::IxcStatus returnedFirmwareStatus,
+    const std::vector<dsp_fw::ModuleEntry> &returnedEntries)
+{
+    std::size_t moduleInfoSize = ModulesInfoHelper::getAllocationSize();
+
+    /** Filling a ModulesInfo structure with the supplied module entries */
+    TypedBuffer<dsp_fw::ModulesInfo> buffer(moduleInfoSize);
+    buffer->module_count = static_cast<uint32_t>(returnedEntries.size());
+    for (std::size_t i = 0; i < returnedEntries.size(); ++i) {
+        buffer->module_info[i] = returnedEntries[i];
+    }
+
+    addGetModuleParameterCommand<dsp_fw::ModulesInfo>(dsp_fw::MODULES_INFO_GET,
+        buffer, returnedDriverStatus, returnedFirmwareStatus);
+}
+
+/* log parameters methods */
+
+void MockedDeviceCommands::addGetLogParametersCommand(NTSTATUS returnedStatus,
+    const driver::FwLogsState &returnedState)
+{
+    /* Expected buffer, used as both expected input AND output buffer */
+    TinyCmdLogParameterIoctl expected;
+
+    /* Returned output buffer*/
+    TinyCmdLogParameterIoctl returned(expected);
+
+    /* Result code */
+    returned.getTinyCmd().Body.Status = returnedStatus;
+    if (returnedStatus == STATUS_SUCCESS) {
+
+        /* Setting returned log state content if the driver returns success */
+        returned.getFwLogsState() = returnedState;
+    }
+
+    /* Adding entry */
+    mDevice.addIoctlEntry(IOCTL_CMD_APP_TO_AUDIODSP_TINY_GET, &expected.getBuffer(),
+        &expected.getBuffer(), &returned.getBuffer(), true);
+}
+
+void MockedDeviceCommands::addSetLogParametersCommand(NTSTATUS returnedStatus,
+    const driver::FwLogsState &expectedState)
+{
+    /* Expected buffer, used as both expected input AND output buffer */
+    TinyCmdLogParameterIoctl expected;
+
+    /* Setting expected log state content */
+    expected.getFwLogsState() = expectedState;
+
+    /* Returned output buffer*/
+    TinyCmdLogParameterIoctl returned(expected);
+
+    /* Result code */
+    returned.getTinyCmd().Body.Status = returnedStatus;
+
+    /* Adding entry */
+    mDevice.addIoctlEntry(IOCTL_CMD_APP_TO_AUDIODSP_TINY_SET, &expected.getBuffer(),
+        &expected.getBuffer(), &returned.getBuffer(), true);
+}
+
+}
+}
+}
