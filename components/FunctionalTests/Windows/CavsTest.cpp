42ab29e23eeac833a72ffbe25a50310bfc2ae3d7
Thomas Cahuzac <thomasx.cahuzac@intel.com>
2015-07-03 14:28:12 +0200

    Using the test framework into the functional tests
    Rather than duplicating code, the functional test
    should use the test framework.
    
    This patch does it.
    
    Change-Id: I2287a3d5c628f5f2422070a93aa5b1c79ba1416a
    Signed-off-by: Thomas Cahuzac <thomasx.cahuzac@intel.com>

--- components/FunctionalTests/Windows/CavsTest.cpp
+++ components/FunctionalTests/Windows/CavsTest.cpp
@@ -20,16 +20,45 @@
 ********************************************************************************
 */
 
-#include "Windows/IoctlHelper.hpp"
 #include "Core/DebugAgent.hpp"
 #include "TestCommon/HttpClientSimulator.hpp"
 #include "cAVS/Windows/DeviceInjectionDriverFactory.hpp"
 #include "cAVS/Windows/MockedDevice.hpp"
+#include "cAVS/Windows/MockedDeviceCommands.hpp"
 #include "catch.hpp"
 
 using namespace debug_agent::core;
 using namespace debug_agent::cavs;
 using namespace debug_agent::test_common;
+
+static const std::size_t ModuleUIDSize = 4;
+using ModuleUID = uint32_t[ModuleUIDSize];
+
+/** Defining some module uuids... */
+const ModuleUID Module0UID = { 1, 2, 3, 4 };
+const ModuleUID Module1UID = { 11, 12, 13, 14 };
+
+/** Helper function to set a module entry */
+void setModuleEntry(dsp_fw::ModuleEntry &entry, const std::string &name,
+    const ModuleUID &uuid)
+{
+    /* Setting name */
+    assert(name.size() <= sizeof(entry.name));
+    for (std::size_t i = 0; i < sizeof(entry.name); i++) {
+        if (i < name.size()) {
+            entry.name[i] = name[i];
+        }
+        else {
+            /* Filling buffer with 0 after name end */
+            entry.name[i] = 0;
+        }
+    }
+
+    /* Setting GUID*/
+    for (std::size_t i = 0; i < ModuleUIDSize; i++) {
+        entry.uuid[i] = uuid[i];
+    }
+}
 
 
 TEST_CASE("DebugAgent: module entries")
@@ -37,8 +66,24 @@ TEST_CASE("DebugAgent: module entries")
     /* Creating the mocked device */
     std::unique_ptr<windows::MockedDevice> device(new windows::MockedDevice());
 
-    /* Filling the test vector */
-    IoctlHelper::addModuleEntryCommand(*device);
+    /* Setting the test vector
+     * ----------------------- */
+
+    windows::MockedDeviceCommands commands(*device);
+
+    /* Creating 2 module entries */
+    std::vector<dsp_fw::ModuleEntry> returnedEntries(2);
+    setModuleEntry(returnedEntries[0], "module_0", Module0UID);
+    setModuleEntry(returnedEntries[1], "module_1", Module1UID);
+
+    /* Adding the "get module entries" command to the test vector */
+    commands.addGetModuleEntriesCommand(
+        STATUS_SUCCESS,
+        dsp_fw::Message::IxcStatus::ADSP_IPC_SUCCESS,
+        returnedEntries);
+
+    /* Now using the mocked device
+    * --------------------------- */
 
     /* Creating the factory that will inject the mocked device */
     windows::DeviceInjectionDriverFactory driverFactory(std::move(device));
@@ -49,6 +94,7 @@ TEST_CASE("DebugAgent: module entries")
     /* Creating the http client */
     HttpClientSimulator client("localhost");
 
+    /* The expected content is an html table describing the mapping uuid<-> module_id */
     std::string expectedContent(
         "<p>Module type count: 2</p>"
         "<table border='1'><tr><td>name</td><td>uuid</td><td>module id</td></tr>"
@@ -56,7 +102,7 @@ TEST_CASE("DebugAgent: module entries")
         "<tr><td>module_1</td><td>0000000b0000000c0000000d0000000e</td><td>1</td></tr>"
         "</table>");
 
-    /* Doing the request */
+    /* Doing the request on the "/cAVS/module/entries" URI */
     client.request(
         "/cAVS/module/entries",
         HttpClientSimulator::Verb::Get,
