400f376b0b4ceb624a50874e1c1d1a8a0c6f55e3
Thomas Cahuzac <thomasx.cahuzac@intel.com>
2015-07-16 18:33:29 +0200

    Introducing WppController
    The WppController pilots real time
    wpp log session.
    
    This patch introduces it.
    
    Change-Id: Ic10044587c3cae5257f341a571314ee197830603
    Signed-off-by: Thomas Cahuzac <thomasx.cahuzac@intel.com>

---
+++ components/cAVS/include/cAVS/Windows/WppController.hpp
@@ -0,0 +1,143 @@
+/*
+********************************************************************************
+*                              INTEL CONFIDENTIAL
+*   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+*   The source code contained  or  described herein and all documents related to
+*   the source code ("Material") are owned by Intel Corporation or its suppliers
+*   or licensors.  Title to the  Material remains with  Intel Corporation or its
+*   suppliers and licensors. The Material contains trade secrets and proprietary
+*   and  confidential  information of  Intel or its suppliers and licensors. The
+*   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+*   treaty  provisions. No part of the Material may be used, copied, reproduced,
+*   modified, published, uploaded, posted, transmitted, distributed or disclosed
+*   in any way without Intel's prior express written permission.
+*   No license  under any  patent, copyright, trade secret or other intellectual
+*   property right is granted to or conferred upon you by disclosure or delivery
+*   of the Materials,  either expressly, by implication, inducement, estoppel or
+*   otherwise.  Any  license  under  such  intellectual property  rights must be
+*   express and approved by Intel in writing.
+*
+********************************************************************************
+*/
+#pragma once
+
+#include "cAVS/Windows/WppCommon.hpp"
+#include "cAVS/Windows/WindowsTypes.hpp"
+#include <evntrace.h>
+#include <mutex>
+#include <stdexcept>
+
+
+namespace debug_agent
+{
+namespace cavs
+{
+namespace windows
+{
+
+/** This class pilots wpp real time log session.
+ *
+ * Produced events can be consumed using the WppConsumer class.
+ *
+ * Notes:
+ * - when start() is called, if a previous session exists, it is stopped.
+ * - this class is thread safe, start() and stop() can be called concurrently
+ */
+class WppController final
+{
+public:
+    class Exception : public std::logic_error
+    {
+    public:
+        Exception(const std::string &msg) : std::logic_error(msg.c_str()) {}
+    };
+
+    WppController();
+    ~WppController();
+
+    /* Start the session.
+     *
+     * if a previous session exists, it is stopped.
+     * @throw WppController::Exception
+     */
+    void start();
+
+    void stop() NOEXCEPT;
+
+private:
+    /* Values defined by
+     * https://msdn.microsoft.com/en-us/library/windows/desktop/aa364160(v=vs.85).aspx
+     *
+     * (ClientContext member)
+     */
+    enum ClientContextValues {
+        PerformanceCounter = 1,
+        SystemTime = 2,
+        CPUCycleCount = 3
+    };
+
+    /** Flag used to filter firmware logs
+     *
+     * Wpp use flags to specify the log source: (from SW\HDAudioOEDrv\HDAudioOEDrv\Log.h)
+     *
+     * #define WPP_CONTROL_GUIDS \
+     *     WPP_DEFINE_CONTROL_GUID(\
+     *     BusCtrlGuid, (b3a109ec, 1cb3, 4947, 95ed, 431033eeb1b4), \
+     *         WPP_DEFINE_BIT(POWER)            \ Power: bit 0,    flag = 1 << 0 = 1
+     *         WPP_DEFINE_BIT(OE_BUS)           \ Bus: bit 1,      flag = 1 << 1 = 2
+     *         WPP_DEFINE_BIT(OE_FW)            \ Firmware: bit 2, flag = 1 << 2 = 4
+     *         WPP_DEFINE_BIT(OE_HW)            \ Hardware: bit 3, flag = 1 << 3 = 8
+     *         ...
+     *
+     * Therefore the firmware flag is  1 << 2 = 4
+     */
+    static const ULONG fwLogFlag = 1 << 2;
+
+    /** Wpp requires a structure that contains an EVENT_TRACE_PROPERTIES instance followed by
+     * the session name, then the log file name.
+     *
+     * @see https://msdn.microsoft.com/en-us/library/windows/desktop/aa364160(v=vs.85).aspx
+     * (description of member "BufferSize")
+     */
+    struct TraceProperties
+    {
+        EVENT_TRACE_PROPERTIES properties;
+
+        /**
+        * from https://msdn.microsoft.com/en-us/library/windows/desktop/aa364103(v=vs.85).aspx:
+        *
+        * "You can use the maximum session name (1024 characters) and maximum log file name
+        * (1024 characters) lengths to calculate the buffer size and offsets if not known."
+        */
+        static const std::size_t sessionNameMaxLen = 1024;
+        static const std::size_t logFileNameMaxLen = 1024;
+
+        char sessionName[sessionNameMaxLen];
+        char logFileName[logFileNameMaxLen];
+
+        TraceProperties();
+
+        /* Set initial structure content:
+         * The whole content is firstly set to zero, then the properties.Wnode.BufferSize field
+         * is initialized.
+         */
+        void init();
+    };
+
+    WppController(const WppController &) = delete;
+    WppController &operator=(const WppController&) = delete;
+
+    /* If a previous session exists, delete it. */
+    void cleanupOldSession();
+
+    /* Must be called in a locked context. */
+    void stopLocked();
+
+    TRACEHANDLE mHandle;
+    std::mutex mHandleMutex;
+    TraceProperties mTraceProperties;
+};
+
+}
+}
+}
---
+++ components/cAVS/src/Windows/WppController.cpp
@@ -0,0 +1,213 @@
+/*
+********************************************************************************
+*                              INTEL CONFIDENTIAL
+*   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+*   The source code contained  or  described herein and all documents related to
+*   the source code ("Material") are owned by Intel Corporation or its suppliers
+*   or licensors.  Title to the  Material remains with  Intel Corporation or its
+*   suppliers and licensors. The Material contains trade secrets and proprietary
+*   and  confidential  information of  Intel or its suppliers and licensors. The
+*   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+*   treaty  provisions. No part of the Material may be used, copied, reproduced,
+*   modified, published, uploaded, posted, transmitted, distributed or disclosed
+*   in any way without Intel's prior express written permission.
+*   No license  under any  patent, copyright, trade secret or other intellectual
+*   property right is granted to or conferred upon you by disclosure or delivery
+*   of the Materials,  either expressly, by implication, inducement, estoppel or
+*   otherwise.  Any  license  under  such  intellectual property  rights must be
+*   express and approved by Intel in writing.
+*
+********************************************************************************
+*/
+#include "cAVS/Windows/WppController.hpp"
+#include "cAVS/Windows/Buffer.hpp"
+#include "cAVS/Windows/LastError.hpp"
+#include <iostream>
+
+namespace debug_agent
+{
+namespace cavs
+{
+namespace windows
+{
+
+/* GUID for audio dsp log control */
+static const GUID AudioDspLogControlGuid =
+{ 0xB3A109EC, 0x1CB3, 0x4947, { 0x95, 0xED, 0x43, 0x10, 0x33, 0xEE, 0xB1, 0xB4 } };
+
+WppController::TraceProperties::TraceProperties()
+{
+    init();
+}
+
+void WppController::TraceProperties::init()
+{
+    memset(this, 0, sizeof(TraceProperties));
+
+    WNODE_HEADER &header = properties.Wnode;
+
+    /* from https://msdn.microsoft.com/en-us/library/windows/desktop/aa364160(v=vs.85).aspx:
+    *
+    * "BufferSize: Total size of memory allocated, in bytes, for the event tracing session
+    * properties. The size of memory must include the room for the EVENT_TRACE_PROPERTIES
+    * structure plus the session name string and log file name string that follow the structure
+    * in memory."
+    *
+    * The TraceProperties structure (defined by us) contains EVENT_TRACE_PROPERTIES + session name
+    * + log file name;
+    */
+    header.BufferSize = sizeof(TraceProperties);
+}
+
+WppController::WppController() : mHandle(INVALID_PROCESSTRACE_HANDLE)
+{
+}
+
+WppController::~WppController()
+{
+    /* Ensure that the session is stopped */
+    stop();
+}
+
+void WppController::start()
+{
+    std::lock_guard<std::mutex> locker(mHandleMutex);
+
+    if (mHandle != INVALID_PROCESSTRACE_HANDLE) {
+        /* Already started */
+        return;
+    }
+
+    cleanupOldSession();
+
+    /* Reset session properties */
+    mTraceProperties.init();
+
+    /* Setting session name */
+    strcpy_s(mTraceProperties.sessionName, sizeof(mTraceProperties.sessionName), wppSessionName);
+
+    /* Don't set logFileName member because it is not used, and it has already set to 0
+     * within the TraceProperties constructor. */
+
+    EVENT_TRACE_PROPERTIES &properties = mTraceProperties.properties;
+    WNODE_HEADER &header = properties.Wnode;
+
+    /* Note: There is no need to set the header.guid field:
+     * from https://msdn.microsoft.com/en-us/library/windows/desktop/aa364160(v=vs.85).aspx:
+     *
+     * "If you start a session that is not a kernel logger or private logger session, you do not
+     * have to specify a session GUID. If you do not specify a GUID, ETW creates one for you.
+     * You need to specify a session GUID only if you want to change the default permissions
+     * associated with a specific session. For details, see the EventAccessControl function.
+     */
+
+    /* Using the PerformanceCounter value in order to obtain high-resolution timestamps */
+    properties.Wnode.ClientContext = ClientContextValues::PerformanceCounter;
+
+    /* from https://msdn.microsoft.com/en-us/library/windows/desktop/aa364160(v=vs.85).aspx:
+     *
+     * "Must contain WNODE_FLAG_TRACED_GUID to indicate that the structure contains event tracing
+     * information."
+     */
+    properties.Wnode.Flags = WNODE_FLAG_TRACED_GUID;
+
+    /*
+     * EVENT_TRACE_REAL_TIME_MODE: Delivers the events to consumers in real-time. Events are
+     * delivered when the buffers are flushed, not at the time the provider writes the event.
+     *
+     * EVENT_TRACE_USE_LOCAL_SEQUENCE: Uses sequence numbers that are unique only for an individual
+     * event tracing session.
+     *
+     * EVENT_TRACE_NO_PER_PROCESSOR_BUFFERING: Writes events that were logged on different
+     * processors to a common buffer. Using this mode can eliminate the issue of events appearing
+     * out of order when events are being published on different processors using system time.
+     * This mode can also eliminate the issue with circular logs appearing to drop events on
+     * multiple processor computers.
+     */
+     properties.LogFileMode = EVENT_TRACE_REAL_TIME_MODE | EVENT_TRACE_USE_LOCAL_SEQUENCE
+        | EVENT_TRACE_NO_PER_PROCESSOR_BUFFERING;
+
+     /* LoggerNameOffset: Offset from the start of the structure's allocated memory to beginning
+      * of the null-terminated string that contains the session name.*/
+     properties.LoggerNameOffset = static_cast<ULONG>(
+         reinterpret_cast<uint8_t*>(&mTraceProperties.logFileName)
+         - reinterpret_cast<uint8_t*>(&mTraceProperties));
+
+     /* LogFileNameOffset: Offset from the start of the structure's allocated memory to beginning
+      * of the null-terminated string that contains the log file name.
+      *
+      * If you do not want to log events to a log file (for example, you specify
+      * EVENT_TRACE_REAL_TIME_MODE only), set LogFileNameOffset to 0.
+      */
+    properties.LogFileNameOffset = 0;
+
+    /* Starting the session */
+    ULONG status = StartTrace(&mHandle, wppSessionName, &properties);
+    if (status != ERROR_SUCCESS)
+    {
+        mHandle = INVALID_PROCESSTRACE_HANDLE;
+        throw Exception("Unable to start session: err=" + std::to_string(status));
+    }
+
+    /* Enabling the audio dsp log provider */
+    status = EnableTrace(
+        TRUE, /* Enable provider */
+        fwLogFlag, /* filter firmware logs */
+        TRACE_LEVEL_VERBOSE, /* retrieve all logs */
+        &AudioDspLogControlGuid,
+        mHandle);
+
+    if (ERROR_SUCCESS != status)
+    {
+        stopLocked();
+        throw Exception("Unable to enable log provider: err=" + std::to_string(status));
+    }
+}
+
+void WppController::stopLocked()
+{
+    /* Must be called in a locked context. */
+
+    if (mHandle == INVALID_PROCESSTRACE_HANDLE) {
+        /* Already stopped */
+        return;
+    }
+
+    ULONG status = StopTrace(mHandle, NULL, &mTraceProperties.properties);
+    if (status != ERROR_SUCCESS)
+    {
+        /* Not throwing exception because nothing can be done here except logging... */
+        std::cout << "Unable to stop session: err=" << status << std::endl;
+    }
+    mHandle = INVALID_PROCESSTRACE_HANDLE;
+}
+
+void WppController::stop()
+{
+    std::lock_guard<std::mutex> locker(mHandleMutex);
+
+    stopLocked();
+}
+
+void WppController::cleanupOldSession()
+{
+    TraceProperties traceProperties;
+
+    /** Looking for an existing session */
+    ULONG status = QueryTraceA(NULL, wppSessionName, &traceProperties.properties);
+    if (status == ERROR_SUCCESS) {
+
+        /* Session exists, stopping it */
+        status = StopTraceA(NULL, wppSessionName, &traceProperties.properties);
+        if (status != ERROR_SUCCESS) {
+            throw Exception("Unable to stop existing session: err=" + std::to_string(status));
+        }
+    }
+    else if (status != ERROR_WMI_INSTANCE_NOT_FOUND) {
+        throw Exception("Unable to query existing session: err=" + std::to_string(status));
+    }
+}
+
+}
+}
+}
