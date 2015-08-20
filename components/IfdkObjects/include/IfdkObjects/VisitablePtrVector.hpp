6ebf2d17f6d5e7bcffe5236df22f15a61c2f9554
Thomas Cahuzac <thomasx.cahuzac@intel.com>
2015-08-20 14:22:12 +0200

    Ifdk objects: introducing visitable collections
    All Ifdk objects will be visitable (see "Visitor
    design pattern), in order to serialize them.
    
    This patch introduces generic visitable collections,
    that will be used by Ifdk business objects.
    
    Change-Id: Iee11b3d2ab594b6c5fe26f43f394b836426b3474
    Signed-off-by: Thomas Cahuzac <thomasx.cahuzac@intel.com>

---
+++ components/IfdkObjects/include/IfdkObjects/VisitablePtrVector.hpp
@@ -0,0 +1,125 @@
+/*
+********************************************************************************
+*                              INTEL CONFIDENTIAL
+*   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+*   The source code contained  or  described herein and all documents related to
+*   the source code ("Material") are owned by Intel Corporation or its suppliers
+*   or licensors.  Title to the  Material remains with  Intel Corporation or its
+*   suppliers and licensors. The Material contains trade secrets and proprietary
+*   and  confidential  information of  Intel or its suppliers and licensors. The
+*   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+*   treaty  provisions. No part of the Material may be used, copied, reproduced,
+*   modified, published, uploaded, posted, transmitted, distributed or disclosed
+*   in any way without Intel's prior express written permission.
+*   No license  under any  patent, copyright, trade secret or other intellectual
+*   property right is granted to or conferred upon you by disclosure or delivery
+*   of the Materials,  either expressly, by implication, inducement, estoppel or
+*   otherwise.  Any  license  under  such  intellectual property  rights must be
+*   express and approved by Intel in writing.
+*
+********************************************************************************
+*/
+
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <assert.h>
+
+namespace debug_agent
+{
+namespace ifdk_objects
+{
+
+/** Generic visitable vector of shared ptr.
+ *
+ * This vector is suitable for polymorphic lists.
+ *
+ * @tparam ElementType base class of elements
+ * @tparam Visitor The used visitor class
+ * @tparam ConstVisitor The used constant visitor class
+ */
+template <class ElementType, class Visitor, class ConstVisitor>
+class VisitablePtrVector final
+{
+public:
+    using ElementPtrVector = std::vector<std::shared_ptr<ElementType>>;
+
+    VisitablePtrVector() = default;
+    explicit VisitablePtrVector(const VisitablePtrVector& other) = default;
+    VisitablePtrVector &operator=(const VisitablePtrVector &other) = default;
+
+    /** The == operator tests pointed value, not the pointer value */
+    bool operator == (const VisitablePtrVector &other) const NOEXCEPT
+    {
+        if (mElementPtrs.size() != other.mElementPtrs.size()) {
+            return false;
+        }
+
+        for (std::size_t i = 0; i < mElementPtrs.size(); ++i) {
+            /* Testing pointer content */
+            if (*mElementPtrs[i] != *other.mElementPtrs[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool operator != (const VisitablePtrVector &other) const NOEXCEPT
+    {
+        return !(*this == other);
+    }
+
+    /* A visitable element should implement the accept() methods (const and non const) */
+
+    void accept(Visitor &visitor)
+    {
+        acceptCommon(*this, visitor);
+    }
+
+    void accept(ConstVisitor &visitor) const
+    {
+        acceptCommon(*this, visitor);
+    }
+
+    void add(ElementType *element)
+    {
+        assert(element != nullptr);
+        mElementPtrs.push_back(std::shared_ptr<ElementType>(element));
+    }
+
+    void add(std::shared_ptr<ElementType> element)
+    {
+        assert(element != nullptr);
+        mElementPtrs.push_back(element);
+    }
+
+    ElementPtrVector &getElements()
+    {
+        return mElementPtrs;
+    }
+
+private:
+    /* This template trick is used to factorize the accept() method for both const and non-const
+     * versions */
+    template <typename T, typename VisitorType>
+    static void acceptCommon(T &me, VisitorType &visitor)
+    {
+        visitor.enter(me);
+
+        for (auto &type : me.mElementPtrs) {
+            type->accept(visitor);
+        }
+
+        visitor.leave();
+    }
+
+    ElementPtrVector mElementPtrs;
+};
+
+}
+}
+
+
+
---
+++ components/IfdkObjects/include/IfdkObjects/VisitableVector.hpp
@@ -0,0 +1,117 @@
+/*
+********************************************************************************
+*                              INTEL CONFIDENTIAL
+*   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+*   The source code contained  or  described herein and all documents related to
+*   the source code ("Material") are owned by Intel Corporation or its suppliers
+*   or licensors.  Title to the  Material remains with  Intel Corporation or its
+*   suppliers and licensors. The Material contains trade secrets and proprietary
+*   and  confidential  information of  Intel or its suppliers and licensors. The
+*   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+*   treaty  provisions. No part of the Material may be used, copied, reproduced,
+*   modified, published, uploaded, posted, transmitted, distributed or disclosed
+*   in any way without Intel's prior express written permission.
+*   No license  under any  patent, copyright, trade secret or other intellectual
+*   property right is granted to or conferred upon you by disclosure or delivery
+*   of the Materials,  either expressly, by implication, inducement, estoppel or
+*   otherwise.  Any  license  under  such  intellectual property  rights must be
+*   express and approved by Intel in writing.
+*
+********************************************************************************
+*/
+
+#pragma once
+
+#include <vector>
+#include <assert.h>
+
+namespace debug_agent
+{
+namespace ifdk_objects
+{
+
+/** Implements a generic visitable vector
+ *
+ * @tparam ElementType class of elements
+ * @tparam Visitor The used visitor class
+ * @tparam ConstVisitor The used constant visitor class
+ */
+template <class ElementType, class Visitor, class ConstVisitor>
+class VisitableVector final
+{
+public:
+    using ElementVector = std::vector<ElementType>;
+
+    VisitableVector() = default;
+    explicit VisitableVector(const VisitableVector& other) = default;
+    VisitableVector &operator=(const VisitableVector &other) = default;
+
+    bool operator == (const VisitableVector &other) const NOEXCEPT
+    {
+        if (mElements.size() != other.mElements.size()) {
+            return false;
+        }
+
+        for (std::size_t i = 0; i < mElements.size(); ++i) {
+            if (mElements[i] != other.mElements[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool operator != (const VisitableVector &other) const NOEXCEPT
+    {
+        return !(*this == other);
+    }
+
+    /* A visitable element should implement the accept() methods (const and non const) */
+
+    void accept(Visitor &visitor)
+    {
+        acceptCommon(*this, visitor);
+    }
+
+    void accept(ConstVisitor &visitor) const
+    {
+        acceptCommon(*this, visitor);
+    }
+
+    void add(const ElementType &element)
+    {
+        mElements.push_back(element);
+    }
+
+    void resize(std::size_t size)
+    {
+        mElements.resize(size);
+    }
+
+    ElementVector &getElements()
+    {
+        return mElements;
+    }
+
+private:
+    /* This template trick is used to factorize the accept() method for both const and non-const
+     * versions */
+    template <typename T, typename VisitorType>
+    static void acceptCommon(T &me, VisitorType &visitor)
+    {
+        visitor.enter(me);
+
+        for (auto &type : me.mElements) {
+            type.accept(visitor);
+        }
+
+        visitor.leave();
+    }
+
+    ElementVector mElements;
+};
+
+}
+}
+
+
