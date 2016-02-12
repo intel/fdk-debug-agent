8bf2f21ae00a4237b0d20c48b50852b7285e1a23
David Wagner <david.wagner@intel.com>
2016-02-12 10:54:36 +0100

    Probes tests: add failure tests
    Check all that all error conditions are properly detected by the get/setState
    and get/setSessionProbes APIs.
    
    Change-Id: I22f95b07883374fe568a99b1b56bf17c090af188
    Signed-off-by: David Wagner <david.wagner@intel.com>

--- components/cAVS/test/Windows/ProberUnitTest.cpp
+++ components/cAVS/test/Windows/ProberUnitTest.cpp
@@ -54,7 +54,22 @@ TEST_CASE_METHOD(Fixture, "Probing: set/getState", "[prober]")
         CHECK(prober.getState() == state);
     }
 
-    // TODO: error cases
+    // Have the prober throw on an illegal state (arguments passed to
+    // addSetProbeStateCommand are irrelevant)
+    CHECK_THROWS_AS_MSG(prober.setState(static_cast<Me::State>(42)), Me::Exception,
+                        "Wrong state value (42).")
+
+    // Have the driver return an error (the ProbeState argument is irrelevant)
+    commands.addSetProbeStateCommand(true, STATUS_FLOAT_DIVIDE_BY_ZERO, driver::ProbeState::Idle);
+    CHECK_THROWS_AS_MSG(prober.setState(Me::State::Idle), Me::Exception,
+                        "Driver returns invalid status: " +
+                            std::to_string(static_cast<uint32_t>(STATUS_FLOAT_DIVIDE_BY_ZERO)));
+
+    // Have the OS make the ioctl() call fail (driver status and ProbeState
+    // arguments are irrelevant)
+    commands.addSetProbeStateCommand(false, STATUS_SUCCESS, driver::ProbeState::Idle);
+    CHECK_THROWS_AS_MSG(prober.setState(Me::State::Idle), Me::Exception,
+                        "TinySet error: OS says that io control has failed.");
 }
 TEST_CASE_METHOD(Fixture, "Probing: set/getSessionProbes", "[prober]")
 {
@@ -88,6 +103,53 @@ TEST_CASE_METHOD(Fixture, "Probing: set/getSessionProbes", "[prober]")
     commands.addGetProbeConfigurationCommand(true, STATUS_SUCCESS, sampleDriverConfig);
     CHECK_NOTHROW(prober.getSessionProbes() == sampleCavsConfig);
 
-    // TODO: error cases
-    // TODO: invalid probe point ids
+    // Check that illegal values are properly detected
+    std::vector<Me::SessionProbes> illegalCavsConfigs;
+    uint32_t illegalModuleId = 1 << driver::ProbePointId::moduleIdSize;
+    uint32_t illegalInstanceId = 1 << driver::ProbePointId::instanceIdSize;
+    Me::ProbeType illegalType = static_cast<Me::ProbeType>(1 << driver::ProbePointId::typeSize);
+    uint32_t illegalIndex = 1 << driver::ProbePointId::indexSize;
+
+    for (auto illegal : {Me::ProbePointId{illegalModuleId, 0, Me::ProbeType::Input, 0},
+                         Me::ProbePointId{0, illegalInstanceId, Me::ProbeType::Input, 0},
+                         Me::ProbePointId{0, 0, illegalType, 0},
+                         Me::ProbePointId{0, 0, Me::ProbeType::Input, illegalIndex}}) {
+        Me::SessionProbes illegalCavsConfig{{true, illegal, Me::ProbePurpose::Inject}};
+        // The rest of the vector is just here as mandatory padding
+        illegalCavsConfig.insert(end(illegalCavsConfig), 7,
+                                 {true, {0, 0, Me::ProbeType::Input, 0}, Me::ProbePurpose::Inject});
+
+        illegalCavsConfigs.emplace_back(illegalCavsConfig);
+    }
+
+    using std::to_string;
+    CHECK_THROWS_AS_MSG(prober.setSessionProbes(illegalCavsConfigs[0]), Me::Exception,
+                        "Module id too large (" + to_string(illegalModuleId) + ").");
+    CHECK_THROWS_AS_MSG(prober.setSessionProbes(illegalCavsConfigs[1]), Me::Exception,
+                        "Instance id too large (" + to_string(illegalInstanceId) + ").");
+    CHECK_THROWS_AS_MSG(prober.setSessionProbes(illegalCavsConfigs[2]), Me::Exception,
+                        "Type too large (" + to_string(static_cast<uint32_t>(illegalType)) + ").");
+    CHECK_THROWS_AS_MSG(prober.setSessionProbes(illegalCavsConfigs[3]), Me::Exception,
+                        "Pin index too large (" + to_string(illegalIndex) + ").");
+
+    Me::SessionProbes illegalPurposeCavsConfig{
+        {true, {0, 0, Me::ProbeType::Input, 0}, static_cast<Me::ProbePurpose>(3)}};
+    // The rest of the vector is just here as mandatory padding
+    illegalPurposeCavsConfig.insert(
+        end(illegalPurposeCavsConfig), 7,
+        {true, {0, 0, Me::ProbeType::Input, 0}, Me::ProbePurpose::Inject});
+    CHECK_THROWS_AS_MSG(prober.setSessionProbes(illegalPurposeCavsConfig), Me::Exception,
+                        "Wrong purpose value (3).");
+
+    // Have the driver return an error (the ProbeState argument is irrelevant)
+    commands.addSetProbeConfigurationCommand(true, STATUS_FLOAT_DIVIDE_BY_ZERO, sampleDriverConfig);
+    CHECK_THROWS_AS_MSG(prober.setSessionProbes(sampleCavsConfig), Me::Exception,
+                        "Driver returns invalid status: " +
+                            std::to_string(static_cast<uint32_t>(STATUS_FLOAT_DIVIDE_BY_ZERO)));
+
+    // Have the OS make the ioctl() call fail (driver status and ProbeState
+    // arguments are irrelevant)
+    commands.addSetProbeConfigurationCommand(false, STATUS_SUCCESS, sampleDriverConfig);
+    CHECK_THROWS_AS_MSG(prober.setSessionProbes(sampleCavsConfig), Me::Exception,
+                        "TinySet error: OS says that io control has failed.");
 }
