1e4d61e8df603f42aa9b004cad84c028ac4a20a1
Thomas Cahuzac <thomasx.cahuzac@intel.com>
2016-03-08 16:59:42 +0100

    Improving ensureNonBlocking() method
    This test method intends to ensure that a function
    call doesn't block more than a period of time. But currently
    ensureNonBlocking() uses std::thread, therefore exceptions
    thrown in the thread context are not forwarded to the client
    thread.
    
    So this patch simplifies ensureNonBlocking() by using std::async.
    
    Change-Id: I0327db483622a14a58eb3b719aabbabcdb64d91f
    Signed-off-by: Thomas Cahuzac <thomasx.cahuzac@intel.com>

--- components/TestCommon/include/TestCommon/ThreadHelpers.hpp
+++ components/TestCommon/include/TestCommon/ThreadHelpers.hpp
@@ -1,7 +1,7 @@
 /*
 ********************************************************************************
 *                              INTEL CONFIDENTIAL
-*   Copyright(C) 2015 Intel Corporation. All Rights Reserved.
+*   Copyright(C) 2016 Intel Corporation. All Rights Reserved.
 *   The source code contained  or  described herein and all documents related to
 *   the source code ("Material") are owned by Intel Corporation or its suppliers
 *   or licensors.  Title to the  Material remains with  Intel Corporation or its
@@ -21,9 +21,7 @@
 */
 
 #pragma once
-#include <thread>
-#include <condition_variable>
-#include <mutex>
+#include <future>
 #include <stdexcept>
 #include <functional>
 
@@ -46,58 +44,33 @@ public:
      * The supplied function is executed into a dedicated thread.
      * - if the function returns before the timeout, the associated thread ends and is joined.
      * - if the timemout is reached, the ensureNonBlocking method throws an exception,
-     *   and the thread is not joined, and may run undefinitely, leading to potential memory leaks.
+     *   and the thread is not joined, and may run undefinitely, leading to memory leaks.
      *
-     * That's why this method should only be used in test context.
+     * That's why this method SHALL ONLY BE USED IN TEST CONTEXT.
      */
     static void ensureNonBlocking(std::function<void()> function, std::size_t timeoutMs = 5000)
     {
-        std::mutex mutex;
-        std::condition_variable var;
-        std::cv_status status;
+        using Future = std::future<void>;
 
-        /* Starting a locked context */
-        std::unique_lock<std::mutex> locker(mutex);
+        // Starting lambda asynchroneously
+        auto future = std::make_unique<Future>(std::async(std::launch::async, function));
 
-        /* Starting the thread that will execute the user function. The operator 'new' is used
-         * explicitly since the Thread cannot be deleted in case we have no other choice than
-         * abandon it. */
-        std::thread *thread =
-            new std::thread(functionThread, function, std::ref(mutex), std::ref(var));
+        // Waiting with timeout
+        std::future_status status = future->wait_for(std::chrono::milliseconds(timeoutMs));
 
-        /* The mutex will be released by the wait_for() method, and only after that, the thread
-         * that executes the user function will be able to notify the condition variable, making
-         * return the wait_for() method.
-         */
-        status = var.wait_for(locker, std::chrono::milliseconds(timeoutMs));
-
-        if (status == std::cv_status::timeout) {
-            /* The thread is not terminated, so abandoning it and throwing an exception */
-            throw Exception("Unable to join thread: timeout reached.");
-        } else {
-            /* The function has returned before the timeout: joining and deleting the thread. */
-            thread->join();
-            delete thread;
+        if (status == std::future_status::timeout) {
+            // Timeout is reached: not deleting the "Future" instance in order to not join
+            // the underlying thread that may never end.
+            future.release();
+            throw Exception("Timeout reached (" + std::to_string(timeoutMs) + "ms)");
         }
+
+        // Calling get() in order to throw exception potentially raised by the lambda.
+        future->get();
     }
 
 private:
     ThreadHelpers() = delete;
-
-    /** Launched in a dedicated thread */
-    static void functionThread(std::function<void()> function, std::mutex &mutex,
-                               std::condition_variable &var)
-    {
-        /* Executing user function */
-        function();
-
-        /* Entering into a locked context, in this way it is ensured that the client thread is
-        locked on var.wait_for() */
-        std::unique_lock<std::mutex> locker(mutex);
-
-        /* Waking up the client thread locked on var.wait_for() */
-        var.notify_one();
-    }
 };
 }
 }
